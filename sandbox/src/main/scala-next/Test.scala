73b0144d552af6a0d75811b760d79eee189c2ffb
Alexander Mordovskiy <iammorjj@gmail.com>
2026-02-06 13:34:29 +0100

    GCBenchBenchmark using SafeZone
    Run from repo's root on dev branch `ENABLE_EXPERIMENTAL_COMPILER=true sbt "sandbox3_next/compile"`
    
    Failed to compile `alloc(new Node(makeTree(iDepth - 1), makeTree(iDepth - 1), 0, 0))`
    
    [error] -- [E007] Type Mismatch Error: /Users/a.mordovskiy/scala-native-playground/scala-native/sandbox/src/main/scala-next/Test.scala:13:15
    [error] 13 |    SafeZone { sz ?=>
    [error]    |               ^
    [error]    |Found:    (sz: scala.scalanative.memory.SafeZone^'s1) ?->'s2 Boolean
    [error]    |Required: (scala.scalanative.memory.SafeZone^) ?=> Boolean
    [error]    |
    [error]    |Note that capability `any` is not included in capture set {}.
    [error]    |
    [error]    |where:    ?=> refers to a root capability created in method run when checking argument to parameter f of method apply
    [error]    |          ^   refers to the root capability caps.any
    [error]    |          any is a root capability created in anonymous function of type (using sz: scala.scalanative.memory.SafeZone^'s1): Boolean of parameter parameter sz² of method $anonfun
    [error]    |
    [error] 14 |      class Node(var left: Node^{sz}, var right: Node^{sz}, var i: Int, var j: Int)
    [error] 15 |...
    [error] 85 |      longLivedTree != null && array(1000) == 1.0 / 1000
    [error]    |
    [error]    | longer explanation available when compiling with `-explain`
    [error] one error found
    [error] (sandbox3_next / Compile / compileIncremental) Compilation failed
    [error] Total time: 2 s, completed 6 Feb 2026, 13:37:12

--- sandbox/src/main/scala-next/Test.scala
+++ sandbox/src/main/scala-next/Test.scala
@@ -1,4 +1,105 @@
 import scala.language.experimental.captureChecking
 
-@main def TestExperimental() =
-  println("Hello Scala Next!")
+import scala.{Int, Double, Boolean, Unit, Array}
+import java.lang.String
+
+import scala.scalanative.memory.SafeZone
+import scala.scalanative.memory.SafeZone._
+import scala.scalanative.runtime.SafeZoneAllocator.allocate
+
+
+object GCBenchBenchmark {
+  def run(input: String): Boolean = {
+    SafeZone { sz ?=>
+      class Node(var left: Node^{sz}, var right: Node^{sz}, var i: Int, var j: Int)
+      val kStretchTreeDepth: Int   = 18 // about 16Mb
+      val kLongLivedTreeDepth: Int = 16 // about 4Mb
+      val kArraySize: Int          = 500000 // about 4Mb
+      val kMinTreeDepth: Int       = 4
+      val kMaxTreeDepth: Int       = 16
+
+      // Nodes used by a tree of a given size
+      def treeSize(i: Int): Int = ((1 << (i + 1)) - 1)
+
+      // Number of iterations to use for a given tree depth
+      def numIters(i: Int): Int = 2 * treeSize(kStretchTreeDepth) / treeSize(i)
+
+      // // Build tree top down, assigning to older objects.
+      def populate(iDepth: Int, thisNode: Node^{sz}): Unit =
+        if (iDepth > 0) {
+          thisNode.left = alloc(new Node(null, null, 0, 0))
+          thisNode.right = alloc(new Node(null, null, 0, 0))
+          populate(iDepth - 1, thisNode.left)
+          populate(iDepth - 1, thisNode.right)
+        }
+
+      // Build tree bottom-up
+      def makeTree(iDepth: Int): Node^{sz} =
+        if (iDepth <= 0) {
+          alloc(new Node(null, null, 0, 0))
+        } else {
+          alloc(new Node(makeTree(iDepth - 1), makeTree(iDepth - 1), 0, 0)) // fail
+          // COMPILATION TEST
+          // makeTree(iDepth - 1) // ok
+          // COMPILATION TEST
+        }
+
+      def construction(depth: Int): Unit = {
+        var root: Node^{sz}     = null
+        var tempTree: Node^{sz} = null
+        val iNumIter: Int  = numIters(depth)
+
+        var i = 0
+        while (i < iNumIter) {
+          tempTree = alloc(new Node(null, null, 0, 0))
+          populate(depth, tempTree)
+          tempTree = null
+          i += 1
+        }
+
+        i = 0
+        while (i < iNumIter) {
+          tempTree = makeTree(depth)
+          tempTree = null
+          i += 1
+        }
+      }
+
+      // // Start method logic inlined
+      var root: Node^{sz}          = null
+      var longLivedTree: Node^{sz} = null
+      var tempTree: Node^{sz}      = null
+
+      // // Stretch the memory space quickly
+      tempTree = makeTree(kStretchTreeDepth)
+      tempTree = null
+
+      // Create a long lived object
+      longLivedTree = alloc(new Node(null, null, 0, 0))
+      populate(kLongLivedTreeDepth, longLivedTree)
+
+      // // Create long-lived array, filling half of it
+      val array = alloc(new Array[Double](kArraySize))
+      var i     = 0
+      while (i < kArraySize / 2) {
+        array(i) = 1.0 / i
+        i += 1
+      }
+
+      i = kMinTreeDepth
+      while (i <= kMaxTreeDepth) {
+        construction(i)
+        i += 2
+      }
+
+      // Return the result
+      longLivedTree != null && array(1000) == 1.0 / 1000
+    }
+  }
+
+}
+
+@main def TestExperimental() = {
+  println("Hi Alex!")
+  println(GCBenchBenchmark.run(""))
+}
